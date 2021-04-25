6da6dda0473858e6e70e947d549437193ee4070c
Lucas Moran <InspiredIdealist@users.noreply.github.com>
2021-04-25 14:24:49 -0400

    initial site w/ construction sign

---
+++ src/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hi, I'm Luke.
+        🚧Site Under Construction...🚧
+      </header>
+    </div>
+  );
+}
+
+export default App;
